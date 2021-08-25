fa62ceaf29055f8c258d3fb7a8f56aef15d27409
Frugan <dev@frugan.it>
2021-08-25 11:16:15 +0200

    Added h5bp

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,23 @@ set -o pipefail
 cd /opt/bitnami/nginx/conf;
 
 #https://stackoverflow.com/a/12569731/3929620
-git clone https://github.com/h5bp/server-configs-nginx.git h5bp;
+git clone https://github.com/h5bp/server-configs-nginx.git;
+
+if [ -f "server-configs-nginx/mime.types" ]; then
+  mv server-configs-nginx/mime.types .;
+fi
+
+if [ -d "server-configs-nginx/h5bp" ]; then
+  mv server-configs-nginx/h5bp .;
+fi
+
+rm -Rf server-configs-nginx;
+
+
+#FIXED - [emerg] "gzip" directive is duplicate...
+sed -i \
+  -e 's/^\(\s*gzip.*\)/#\1/' \
+  nginx.conf;
 
 
 ####
