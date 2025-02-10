354769ab6a451907efe7fe4eb8de3515968703bc
Frugan <dev@frugan.it>
2025-02-10 17:26:37 +0100

    feat(h5bp): fallback h5bp repo

--- 1.22/entrypoint.sh
+++ 1.22/entrypoint.sh
@@ -6,22 +6,28 @@ set -o pipefail
 #set -o xtrace # Uncomment this line for debugging purpose
 
 #### h5bp
+REPO_NAME="h5bp/server-configs-nginx"
+REPO_URL="https://github.com/$REPO_NAME.git"
+FALLBACK_DIR="${NGINX_H5BP_FALLBACK_DIR:-/app/conf/$REPO_NAME}"
+DEST_DIR="/opt/bitnami/nginx/conf"
 
-cd /opt/bitnami/nginx/conf
-
-#https://stackoverflow.com/a/12569731/3929620
-git clone https://github.com/h5bp/server-configs-nginx.git
-
-if [ -f "server-configs-nginx/mime.types" ]; then
-	cp -r server-configs-nginx/mime.types .
+if [ -d "$FALLBACK_DIR/.git" ]; then
+	cd "$FALLBACK_DIR"
+	git pull >/dev/null 2>&1 || true
+else
+	#https://stackoverflow.com/a/12569731/3929620
+	git clone "$REPO_URL" "$FALLBACK_DIR" >/dev/null 2>&1 || true
 fi
 
-if [ -d "server-configs-nginx/h5bp" ]; then
-	cp -r server-configs-nginx/h5bp .
+if [ -f "$FALLBACK_DIR/mime.types" ]; then
+	cp "$FALLBACK_DIR/mime.types" "$DEST_DIR"
 fi
 
-rm -Rf server-configs-nginx
+if [ -d "$FALLBACK_DIR/h5bp" ]; then
+	cp -r "$FALLBACK_DIR/h5bp" "$DEST_DIR"
+fi
 
+####
 #FIXED - [emerg] "gzip" directive is duplicate...
 sed -i \
 	-e 's/^\(\s*gzip.*\)/#\1/' \
