174e40f7aafd6fb92c1b04e6a9b71992df9fff7f
Frugan <dev@frugan.it>
2025-02-10 20:49:11 +0100

    fix!: fix $CONF_DIR in entrypoint

--- 1.24/entrypoint.sh
+++ 1.24/entrypoint.sh
@@ -5,11 +5,12 @@ set -o nounset
 set -o pipefail
 #set -o xtrace # Uncomment this line for debugging purpose
 
+CONF_DIR="/opt/bitnami/nginx/conf"
+
 #### h5bp
 REPO_NAME="h5bp/server-configs-nginx"
 REPO_URL="https://github.com/$REPO_NAME.git"
 FALLBACK_DIR="${NGINX_H5BP_FALLBACK_DIR:-/app/conf/$REPO_NAME}"
-DEST_DIR="/opt/bitnami/nginx/conf"
 
 if [ -d "$FALLBACK_DIR/.git" ]; then
 	cd "$FALLBACK_DIR"
@@ -20,23 +21,23 @@ else
 fi
 
 if [ -f "$FALLBACK_DIR/mime.types" ]; then
-	cp "$FALLBACK_DIR/mime.types" "$DEST_DIR"
+	cp "$FALLBACK_DIR/mime.types" "$CONF_DIR"
 fi
 
 if [ -d "$FALLBACK_DIR/h5bp" ]; then
-	cp -r "$FALLBACK_DIR/h5bp" "$DEST_DIR"
+	cp -r "$FALLBACK_DIR/h5bp" "$CONF_DIR"
 fi
 
 ####
 #FIXED - [emerg] "gzip" directive is duplicate...
 sed -i \
 	-e 's/^\(\s*gzip.*\)/#\1/' \
-	nginx.conf
+	"$CONF_DIR/nginx.conf"
 
 {
 	echo ''
-	echo 'include "/opt/bitnami/nginx/conf/custom.d/*.conf";'
-} >>nginx.conf
+	echo "include \"$CONF_DIR/custom.d/*.conf\";"
+} >>"$CONF_DIR/nginx.conf"
 
 ####
 
